54ee940d5db9843edf66abdbed9cdb200bcfc863
Kasia Skowron <kasiaskowron@MacBook-Air-Kasia.local>
2018-05-23 00:16:39 +0200

    6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,24 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        Calculator calculator = new Calculator();
+
+        int result = calculator.addAToB(5, 5);
+
+        if (result == 10) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        int result2 = calculator.substractAFromB(10, 5);
+        if (result2 == 5) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
