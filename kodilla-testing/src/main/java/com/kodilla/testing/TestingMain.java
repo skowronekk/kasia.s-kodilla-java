0f4e8d034c4944d8c1b323dbf526be3fa8417448
Kasia Skowron <kasiaskowron@MacBook-Air-Kasia.local>
2018-05-26 08:15:01 +0200

    6.3

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -4,21 +4,6 @@ import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
 
-        int result = calculator.addAToB(5, 5);
-
-        if (result == 10) {
-            System.out.println("test OK");
-        } else {
-            System.out.println("Error!");
-        }
-
-        int result2 = calculator.substractAFromB(10, 5);
-        if (result2 == 5) {
-            System.out.println("test OK");
-        } else {
-            System.out.println("Error!");
-        }
     }
 }
