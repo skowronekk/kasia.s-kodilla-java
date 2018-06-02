0010a456fa0b2bc001744aa5c4c574ed05b707f9
Kasia Skowron <kasiaskowron@MacBook-Air-Kasia.local>
2018-06-02 14:47:48 +0200

    7.2 corrected

--- kodilla-stream/src/main/java/com/kodilla/stream/immutable/ForumUser.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/immutable/ForumUser.java
@@ -1,11 +1,11 @@
 package com.kodilla.stream.immutable;
 
 public final class ForumUser {
-    private final String userName;
+    private final String username;
     private final String realName;
 
-    public String getUserName() {
-        return userName;
+    public String getUsername() {
+        return username;
     }
 
     public String getRealName() {
@@ -13,7 +13,7 @@ public final class ForumUser {
     }
 
     public ForumUser(final String userName, final String realName) {
-        this.userName = userName;
+        this.username = userName;
         this.realName = realName;
 
     }
