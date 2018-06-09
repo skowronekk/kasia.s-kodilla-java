d994d200ce7ff32573c598bb6e0614821d168bf3
Kasia Skowron <kasiaskowron@MacBook-Air-Kasia.local>
2018-06-09 14:55:14 +0200

    8.2

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -0,0 +1,28 @@
+package com.kodilla.exception.test;
+
+public class FirstChallenge {
+    public double divide(double a, double b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException();
+        }
+        return a / b;
+    }
+
+
+    public static void main(String[] args) {
+
+        FirstChallenge firstChallenge = new FirstChallenge();
+
+        try {
+            double result = firstChallenge.divide(3, 0);
+            System.out.println(result);
+
+        } catch (ArithmeticException e) {
+            System.out.println("You can't divide by 0! Error: " + e);
+
+        } finally {
+            System.out.println("Remember not to divide by 0 ");
+
+        }
+    }
+}
